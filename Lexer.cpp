5450bfcd479cda0f9bedaf1221d4162d66495f0b
Kathy Saad <kathysaad@gmail.com>
2015-06-13 14:37:07 -0700

    Add more structure to the header and cpp file for the lexer

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
+#include <string>
+#include "Lexer.h"
 
 using namespace std;
+
+LexTokPair Lexer::getTokenLexemePair() {
+	int DigitOrRealTable[5][2] = {};
+	int IdentifierTable[5][2] = {};
+}
--- Lexer.h
+++ Lexer.h
@@ -1,1 +1,40 @@
 #pragma once
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct LexTokPair {
+	string token,
+		lexeme;
+};
+
+class Lexer {
+public:
+	Lexer(string);
+	// opens file to be read in
+
+	char getNextChar();
+	// returns the next character in the file
+
+	LexTokPair getTokenLexemePair();
+	// if file input is valid, returns lexeme-token pair
+
+	bool isSeparator(char);
+	// if character is a separator, returns true
+	// this function also checks if the character is a part of the separator "<-"
+
+	bool isOperator(char);
+	// if character is an operator, returns true
+
+	bool isKeyword(string);
+	// if the identifier string is a keyword, returns true
+
+	int findLexemeColumn(char);
+	// depending on what the character is, this function returns the corresponding column of the 2D array
+private:
+	ifstream inFile;
+	string token,
+		lexeme;
+};
