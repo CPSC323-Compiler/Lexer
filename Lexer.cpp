520fcbc3fb72ff030473f2813307d4c93c962fbd
Kathy Saad <kathysaad@gmail.com>
2015-06-13 14:47:30 -0700

    Add comments to machines about starting and accepting states

--- Lexer.cpp
+++ Lexer.cpp
@@ -5,11 +5,16 @@
 using namespace std;
 
 LexTokPair Lexer::getTokenLexemePair() {
+	// Starting state: 1
+	// Acceptance states: 2, 5
 	int DigitOrRealTable[5][2] = { { 2, 3 }, // Row 1
 									{ 2, 4 }, // Row 2
 									{ 3, 3 }, // Row 3
 									{ 5, 3 }, // Row 4
 									{ 5, 3 }, }; // Row 5
+
+	// Starting state: 1
+	// Acceptance states: 2, 4
 	int IdentifierTable[5][2] = { { 2, 3 }, // Row 1
 									{ 4, 5 }, // Row 2
 									{ 3, 3 }, // Row 3
