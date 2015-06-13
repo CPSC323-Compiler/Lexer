a89bfcf65b1b9c4c4378d8f80521450cb7b92c30
Kathy Saad <kathysaad@gmail.com>
2015-06-13 14:44:32 -0700

    Add digit/real and identifier finite state machines

--- Lexer.cpp
+++ Lexer.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 
 LexTokPair Lexer::getTokenLexemePair() {
-	int DigitOrRealTable[5][2] = {};
-	int IdentifierTable[5][2] = {};
+	int DigitOrRealTable[5][2] = { { 2, 3 }, // Row 1
+									{ 2, 4 }, // Row 2
+									{ 3, 3 }, // Row 3
+									{ 5, 3 }, // Row 4
+									{ 5, 3 }, }; // Row 5
+	int IdentifierTable[5][2] = { { 2, 3 }, // Row 1
+									{ 4, 5 }, // Row 2
+									{ 3, 3 }, // Row 3
+									{ 4, 5 }, // Row 4
+									{ 4, 5 }, }; // Row 5
 }
